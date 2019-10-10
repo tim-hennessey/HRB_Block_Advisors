563fe9bb929e150b06c674f4b430bf4e85de97cc
tim.hennessey <Tim.Hennessey@deutschinc.com>
2019-10-10 12:01:50 -0700

    initial commit

---
+++ app/300x250/scripts/TrackingDCM.js
@@ -0,0 +1,27 @@
+var app = app || {};
+
+
+app.Tracking = (function () {
+
+    // --------------------------------------------------------------------------------------
+    // initialize
+    function initialize() {
+        // Create the event.
+        var event = document.createEvent('Event');
+        event.initEvent('READY', true, true);
+        document.dispatchEvent(event);
+        document.getElementById('button-exit').addEventListener('click', handleExit, false);
+    }
+
+    // --------------------------------------------------------------------------------------
+    function handleExit(e) {
+        te_html.click('clickTAG', 'https://www.blockadvisors.com/');
+    }
+
+    // --------------------------------------------------------------------------------------
+    // Publicly accessible methods and properties
+    return {
+        initialize:initialize
+    }
+
+})();
