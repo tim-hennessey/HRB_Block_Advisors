c7624b7675e0af8eb418b526b66c7b63e004f6fb
tim-hennessey <tim.hennessey@deutschinc.com>
2019-10-23 21:16:36 -0700

    Resizes

---
+++ app/160x600_2/scripts/Animation.js
@@ -0,0 +1,54 @@
+var app = app || {};
+
+
+app.Animation = (function () {
+
+	var banner = document.getElementById('banner');
+	var t = TweenMax;
+
+	// --------------------------------------------------------------------------------------
+	// set default properties
+	function initialize() {
+		// DO NOT EDIT: reveals banner once loaded
+		t.set(banner, {opacity:1});
+		t.set(".path", {drawSVG:"0%"});
+		t.set("#cta-over", {opacity:0});
+
+		t.set(".pattern-0", {scale:1, x:53, y:0});
+		t.set(".pattern-1", {scale:1, x:-17, y:483});
+
+		banner.addEventListener('mouseover', handleOver, false);
+		banner.addEventListener('mouseout', handleOut, false);
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Starts the animation
+	function start() {
+		t.to(".path", 4, {drawSVG:"100%", ease:Cubic.easeInOut});
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Stops the animation
+	function stop() {
+		console.log("stopping animation");
+	}
+
+	// --------------------------------------------------------------------------------------
+	function handleOver() {
+		TweenLite.to("#cta-over", 0.15, {opacity:1, ease:Cubic.easeInOut});
+	}
+
+	// --------------------------------------------------------------------------------------
+	function handleOut() {
+		TweenLite.to("#cta-over", 0.3, {opacity:0, ease:Cubic.easeInOut});
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Publicly accessible methods and properties
+	return {
+		initialize:initialize,
+		start:start,
+		stop:stop
+	}
+
+})();
